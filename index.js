be011fc7384f3302d1b3b8bc517e76a164b123dc
RenattoPadilha <rspf@cin.ufpe.br>
2020-08-26 08:06:48 -0300

    Now responsive

---
+++ index.js
@@ -0,0 +1,5 @@
+document.querySelector(".coming-soon").addEventListener("click", alertaSecaoIncompleta);
+
+function alertaSecaoIncompleta(){
+    alert("Essa seção do site ainda está sendo produzida, em breve será adicionada.");
+}
